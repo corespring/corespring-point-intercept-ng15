ada865dac755088f5e37030384d9ce500d26701a
Ed Eustace <ed.eustace@gmail.com>
2017-02-09 10:08:57 +0000

    remove redundant files

--- docs/demo/controllers/entry.js
+++
@@ -1,4 +0,0 @@
-
-         exports['corespring-point-intercept-ng15'] = require('corespring-point-intercept-ng15-controller');
-         exports['corespring-point-intercept-ng15'].version = '../node_modules/corespring-point-intercept-ng15/controller';
-         
